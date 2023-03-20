0f0c4b85f4818136094792b40e9447bcb8f6fbf3
Mashiro <125548786+mashiro00o@users.noreply.github.com>
2023-03-20 08:14:43 +0000

    html and css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,5 +8,25 @@ app = Flask(__name__)
 def home():
   return render_template('index.html', title='Home')
 
+@app.route('/about')
+def about():
+  return render_template('about.html', title='About')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    name = None
+    Class = None
+    house = None
+    message = None
+    if request.method == 'POST':
+        # Handle form submission
+        name = request.form.get('name')
+        Class = request.form.get('Class')
+        house = request.form.get('house')
+        message = request.form.get('message')
+        with open('data.txt', 'a') as file:
+          file.write(name + ' ' + Class + ' '+ house + ' ' + message + '\n')
+        return render_template('thankyou.html', title='Thank You', name=name, Class=Class, house=house, message=message)
+    return render_template('form.html', title='Feedback Form')
 
 app.run(host='0.0.0.0', port=81)
